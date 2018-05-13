b58121c08aa945dcff27f61f6ce065963be95849
Bryce Meyer <bryce13950@gmail.com>
2018-05-13 20:53:08 +0800

    added sign up email

---
+++ code/resources/views/base-mailer.blade.php
@@ -0,0 +1,9 @@
+<html xmlns="http://www.w3.org/1999/html">
+
+<body>
+    <h2>{{ $greeting }}</h2>
+
+    @yield('body')
+</body>
+
+</html>
---
+++ code/resources/views/mailers/sign-up.blade.php
@@ -0,0 +1,25 @@
+@extends('base-mailer')
+
+@section('body')
+
+    <p>
+        Thanks you for taking a look at Project Athenia! We are pretty excited to get this into the hands of everyone. We hope that through the input of user's like you, we can help start a narrative of in what way we could change the way that groups of people organize and discourse with each other.
+    </p>
+    
+    <p>
+        Please take a look at everything in the system, and let us know what we can change or improve. This project is meaningless without your input.
+    </p>
+
+    <p>
+        Also as an additional bit of information. We promise you that will never share your contact information to anyone outside of our team. If you would like to learn more about our privacy policy please click <a href="http://www.projectathenia.com/privacy-policy">here</a>.
+    </p>
+
+    <p>
+        Thanks again,
+    </p>
+
+    <p>
+        Project Athenia
+    </p>
+
+@endsection
