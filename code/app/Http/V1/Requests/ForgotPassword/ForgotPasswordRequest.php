2040f13bad9b29c39a9d0755ef3a2af70e18a5fe
Bryce Meyer <bryce13950@gmail.com>
2018-06-09 18:43:57 +0200

    added requests

---
+++ code/app/Http/V1/Requests/ForgotPassword/ForgotPasswordRequest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\V1\Requests\ForgotPassword;
+
+use App\Http\V1\Requests\BaseUnauthenticatedRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * Class ForgotPasswordRequest
+ * @package App\Http\V1\Requests\ForgotPassword
+ */
+class ForgotPasswordRequest extends BaseUnauthenticatedRequest
+{
+    /**
+     * get the validation rules for when someone has forgotten their password, and needs a token sent to them
+     *
+     * @return array
+     */
+    public function rules() : array
+    {
+        return [
+            'email' => 'required|max:120|email|' . Rule::exists('users', 'email')
+        ];
+    }
+}
---
+++ code/app/Http/V1/Requests/ForgotPassword/ResetPasswordRequest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\V1\Requests\ForgotPassword;
+
+use App\Http\V1\Requests\BaseUnauthenticatedRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * Class ResetPasswordRequest
+ * @package App\Http\V1\Requests\ForgotPassword
+ */
+class ResetPasswordRequest extends BaseUnauthenticatedRequest
+{
+    /**
+     * get the validation rules for resetting a password
+     *
+     * @return array
+     */
+    public function rules() : array
+    {
+        return [
+            'email' => 'required|max:120|email|' . Rule::exists('users', 'email'),
+            'token' => 'required|max:40|' .
+                Rule::exists('password_tokens', 'token') .
+                '|user_owns_token|token_is_not_expired',
+            'password' => 'required|max:120',
+        ];
+    }
+}
