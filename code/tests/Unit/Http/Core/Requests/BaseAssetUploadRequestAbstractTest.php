7d9a61bb2430702b78121f139d6867498a2da3dd
Bryce Meyer <bryce13950@gmail.com>
2020-04-16 19:04:49 +0200

    added test for base asset upload request

---
+++ code/tests/Unit/Http/Core/Requests/BaseAssetUploadRequestAbstractTest.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\Http\Core\Requests;
+
+use App\Http\Core\Requests\BaseAssetUploadRequestAbstract;
+use RuntimeException;
+use Tests\TestCase;
+
+/**
+ * Class BaseAssetUploadRequestAbstractTest
+ * @package Tests\Unit\Http\V1\Requests
+ */
+class BaseAssetUploadRequestAbstractTest extends TestCase
+{
+    public function testValidationDataSetsMimeType()
+    {
+        /** @var BaseAssetUploadRequestAbstract $request */
+        $request = $this->getMockForAbstractClass(BaseAssetUploadRequestAbstract::class);
+
+        $request->replace([
+            'file_contents' => base64_encode('test'),
+        ]);
+
+        $data = callMethod($request, 'validationData');
+
+        $this->assertEquals($data['mime_type'], 'text/plain');
+    }
+
+    public function testValidationDataSkipsMimeType()
+    {
+        /** @var BaseAssetUploadRequestAbstract $request */
+        $request = $this->getMockForAbstractClass(BaseAssetUploadRequestAbstract::class);
+
+        $request->replace([
+            'file_contents' => '<svg></svg>',
+        ]);
+
+        $data = callMethod($request, 'validationData');
+
+        $this->assertNull($data['mime_type']);
+    }
+
+    public function testGetDecodedContentsThrowsException()
+    {
+        $this->expectException(RuntimeException::class);
+
+        /** @var BaseAssetUploadRequestAbstract $request */
+        $request = $this->getMockForAbstractClass(BaseAssetUploadRequestAbstract::class);
+
+        $request->getDecodedContents();
+    }
+
+    public function testGetDecodedContentsReturnsCorrectContents()
+    {
+        /** @var BaseAssetUploadRequestAbstract $request */
+        $request = $this->getMockForAbstractClass(BaseAssetUploadRequestAbstract::class);
+
+        $request->replace([
+            'file_contents' => base64_encode('<svg></svg>'),
+        ]);
+
+        callMethod($request, 'validationData');
+
+        $this->assertEquals('<svg></svg>', $request->getDecodedContents());
+    }
+
+    public function testGetFileMimeTypeThrowsException()
+    {
+        $this->expectException(RuntimeException::class);
+
+        /** @var BaseAssetUploadRequestAbstract $request */
+        $request = $this->getMockForAbstractClass(BaseAssetUploadRequestAbstract::class);
+
+        $request->getFileMimeType();
+    }
+
+    public function testGetFileMimeTypeReturnsCorrectContents()
+    {
+        /** @var BaseAssetUploadRequestAbstract $request */
+        $request = $this->getMockForAbstractClass(BaseAssetUploadRequestAbstract::class);
+
+        $request->replace([
+            'file_contents' => base64_encode('<svg></svg>'),
+        ]);
+
+        callMethod($request, 'validationData');
+
+        $this->assertEquals('image/svg', $request->getFileMimeType());
+    }
+}
